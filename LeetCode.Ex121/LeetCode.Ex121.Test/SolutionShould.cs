b7a1c5969f6a9f7cc66faf30c9e70d2c35036991
Steve Hughes <toxicgorilla@users.noreply.github.com>
2021-01-16 18:17:02 +0000

    Added simple and advanced

--- LeetCode.Ex121/LeetCode.Ex121.Test/SolutionShould.cs
+++ LeetCode.Ex121/LeetCode.Ex121.Test/SolutionShould.cs
@@ -8,6 +8,7 @@ namespace LeetCode.Ex121.Test
         [Theory]
         [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
         [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [InlineData(new int[] { 1, 2 }, 1)]
         public void ShouldReturnCorrectResultWhenSolutionExists(int[] input, int expectedResult)
         {
             var solution = new Solution();
--- LeetCode.Ex121/LeetCode.Ex121/Solution.cs
+++ LeetCode.Ex121/LeetCode.Ex121/Solution.cs
@@ -2,10 +2,54 @@
 {
     public class Solution
     {
+        private int _mode = 1;
+
         public int MaxProfit(int[] prices)
         {
+            return _mode == 0 ? Simple(prices) : Advanced(prices);
+        }
 
-            return 0;
+        private int Simple(int[] prices)
+        {
+            var currentMax = 0;
+            for (var i = 0; i < prices.Length - 1; i++)
+            {
+                for (var j = i + 1; j < prices.Length; j++)
+                {
+                    var profit = prices[j] - prices[i];
+                    if (profit > currentMax)
+                    {
+                        currentMax = profit;
+                    }
+                }
+            }
+
+            return currentMax;
+        }
+
+        private int Advanced(int[] prices)
+        {
+            var currentMaxProfit = 0;
+            var currentLowestPrice = int.MaxValue;
+
+            for (var i = 0; i < prices.Length; i++)
+            {
+                var currentPrice = prices[i];
+                if (currentPrice < currentLowestPrice)
+                {
+                    currentLowestPrice = currentPrice;
+                }
+                else
+                {
+                    var profit = currentPrice - currentLowestPrice;
+                    if (profit > currentMaxProfit)
+                    {
+                        currentMaxProfit = profit;
+                    }
+                }
+            }
+
+            return currentMaxProfit;
         }
     }
 }
